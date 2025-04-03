805f09d20873920eb6dfa85f24372cc53c520ad6
PHIRI DIKELEDI IVY <dphiri9598@gmail.com>
2025-04-03 13:00:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ducument.addEventListener("DOMContentLoaded", function () {const navLinks = document.querySelectorAll("nav ul li a");
+
+navLinks.forEach(link => {
+link.addEventListener("click", function (event) {
+event.preventDefault();
+const sectionId = this.getAttribute("href").substring(1);
+document.getElementById(sectionId).scrollIntoView({
+behavior: "smooth"
+});
+});
+});
+});
